f8e14b81310b3f391094b0464974631a3c231db8
chaitu2729 <109784379+chaitu2729@users.noreply.github.com>
2023-05-12 15:26:26 +0530

    Update declrative.groovy

--- vars/declrative.groovy
+++ vars/declrative.groovy
@@ -10,7 +10,7 @@ def newDeployment(jobname,ip,context)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"  
 }
-def runSelinium(jobaname)
+def runSelenium(jobname)
 {
   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
