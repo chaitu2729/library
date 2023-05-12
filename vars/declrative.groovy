388bff5f49f79a271b0f41c58aaa5601241c51d1
chaitu2729 <109784379+chaitu2729@users.noreply.github.com>
2023-05-12 11:35:55 +0530

    Update declrative.groovy

--- vars/declrative.groovy
+++ vars/declrative.groovy
@@ -6,7 +6,7 @@ def newBuild()
 {
  sh "mvn package"  
 }
-def newDeployment(jobname,ip adress, context)
+def newDeployment(jobname,ipadress,context)
 {
   sh "scp  /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip adress}:/var/lib/tomcat9/webapps/${context}.war"  
 }
